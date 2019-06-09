07314bd6e142aeaac1c42a55fa0e6739c1ef3e97
Heath Ryu <32060873+heathryu@users.noreply.github.com>
2019-06-09 21:00:10 +0100

    Add saveCard business logic to newCardHandler and add route to delete card

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 'use strict';
 
 const Hapi = require('@hapi/hapi');
+const uuid = require('uuid');
+
+const cards = {};
 
 const init = async () => {
   const server = Hapi.server({
@@ -50,8 +53,14 @@ const init = async () => {
   server.route({
     path: '/cards/new',
     method: ['GET', 'POST'],
-    handler: newCardsHandler
+    handler: newCardHandler
   });
+
+  server.route({
+    path: '/cards/{id}',
+    method: 'DELETE',
+    handler: deleteCardHandler
+  })
 
   await server.start();
   console.log('Server running on %s', server.info.uri);
@@ -59,15 +68,38 @@ const init = async () => {
 
 const cardsHandler = (req, h) => {
   return h.file('templates/cards.html');
-}
+};
 
-const newCardsHandler = (req, h) => {
+const newCardHandler = (req, h) => {
   if (req.method === 'get') {
     return h.file('templates/new.html');
   } else if (req.method === 'post') {
+
+    const card = {
+      name: req.payload.name,
+      recipient_email: req.payload.recipient_email,
+      sender_name: req.payload.sender_name,
+      sender_email: req.payload.sender_email,
+      card_image: req.payload.card_image
+    };
+
+    saveCard(card);
+
+    console.log(cards);
+
     return h.redirect('/cards');
   }
-}
+};
+
+const deleteCardHandler = (req, h) => {
+  delete cards[req.params.id];
+};
+
+const saveCard = (card) => {
+  const id = uuid.v1();
+  card.id = id;
+  cards[id] = card;
+};
 
 process.on('unhandledRjection', err => {
   console.log(err);
