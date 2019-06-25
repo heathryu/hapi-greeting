3ed22af54435bc92330d6f31be5f58fef16c74d9
Heath Ryu <32060873+heathryu@users.noreply.github.com>
2019-06-25 22:45:32 +0100

    Refactor the code and rename files to provide cleaner structure

---
+++ server.js
@@ -0,0 +1,105 @@
+'use strict';
+
+const Hapi = require('@hapi/hapi');
+
+const Good = require('@hapi/good');
+const GoodFileReporter = require('./lib/goodFileReporter');
+
+const Routes = require('./lib/routes');
+
+const CardStore = require('./lib/cardStore');
+
+CardStore.initialize();
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 3000,
+    host: 'localhost'
+  });
+
+  await server.register(require('@hapi/inert'));
+  await server.register(require('@hapi/vision'));
+
+  server.views({
+    engines: {
+      html: require('handlebars')
+    },
+    path: 'templates'
+  });
+
+  await server.register({
+    plugin: Good,
+    options: {
+      ops: {
+        interval: 10000
+      },
+      reporters: {
+        processReporter: [
+          {
+            module: '@hapi/good-squeeze',
+            name: 'Squeeze',
+            args: [{ ops: '*' }]
+          },
+          {
+            module: '@hapi/good-squeeze',
+            name: 'SafeJson'
+          },
+          {
+            module: GoodFileReporter,
+            args: ['./logs/process']
+          }
+        ],
+        requestsReporter: [
+          {
+            module: '@hapi/good-squeeze',
+            name: 'Squeeze',
+            args: [{ response: '*' }]
+          },
+          {
+            module: '@hapi/good-squeeze',
+            name: 'SafeJson'
+          },
+          {
+            module: GoodFileReporter,
+            args: ['./logs/requests']
+          }
+        ],
+        errorReporter: [
+          {
+            module: '@hapi/good-squeeze',
+            name: 'Squeeze',
+            args: [{ error: '*' }]
+          },
+          {
+            module: '@hapi/good-squeeze',
+            name: 'SafeJson'
+          },
+          {
+            module: GoodFileReporter,
+            args: ['./logs/errors']
+          }
+        ]
+      }
+    }
+  });
+
+  server.ext('onPreResponse', (req, h) => {
+    if (req.response.isBoom) {
+      return h.view('error', req.response);
+    }
+
+    return h.continue;
+  });
+
+  server.route(Routes);
+
+  await server.start();
+  console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRjection', err => {
+  console.log(err);
+  process.exit(1);
+});
+
+init();
